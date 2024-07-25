4e370c993d17f79892a29d2cf3a68a94911eed24
balaji-89 <balaji89237@gmail.com>
2024-07-25 16:38:51 +0530

    completed sql code

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from web_scrape_src.web_scrap_code import get_route_link_map, extract_bus_info
+from scrapping_src import get_route_link_map, extract_bus_info
+from sql_src import connect_sql_server,add_route, add_bus_info
+
 
 if __name__ == "__main__":
+
+    #scrapping data
     driver = webdriver.Edge()
     driver.maximize_window()
     action = ActionChains(driver)
     route_link_map = get_route_link_map(driver, action)
     bus_info = extract_bus_info(driver, route_link_map)
     driver.quit()
+
+    #saving to sql
+    cursor = connect_sql_server('localhost', 'root', 'password')
+    add_route(cursor,route_link_map)
+    add_bus_info(cursor,bus_info)
---
+++ sql_src/code.py
@@ -0,0 +1,75 @@
+
+import mysql.connector
+
+def connect_sql_server(host_name: str, user_name: str, password: str):
+    try: 
+        con = mysql.connector.connect(
+        host=host_name,
+        user=user_name,
+        password=password
+        )
+        cursor = con.cursor()
+        return cursor
+    except Exception as e:
+        raise e
+
+
+def create_database(database_name: str, cursor, use_database = True):
+    try:
+        query = f"create database if not exists {database_name}"
+        cursor.execute(query)
+        if use_database: 
+            cursor.execute(f'use {database_name}')
+    except Exception as e:
+        raise e
+
+
+def create_table(cursor,table_name:str,attributes:str):
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS {table_name} ({attributes})''' )
+
+
+def insert_values(cursor, table_name:str, attributes:tuple,values:list):
+    cursor.executemany(f'''INSERT INTO {table_name} {str(attributes)} VALUES {str(('%s')*len(attributes))}''', values)
+
+
+
+def add_route(cursor, values:list = None):
+    create_database(database_name='red_bus_scrape',cursor=cursor)
+    create_table(cursor=cursor,table_name='route', attributes='id INT PRIMARY_KEY, route VARCHAR(255), route_link VARCHAR(255)')
+    if values:
+        insert_values(cursor,table_name='route',attributes=('id', 'route', 'route_link'),values=values)
+
+
+def add_bus_info(cursor, values:list = None):
+    create_database(database_name='bus_info',cursor=cursor)
+    create_table(cursor=cursor,table_name='bus_info',attributes='''id INT PRIMARY KEY,
+                                                                    route_id INT,
+                                                                    bus_name VARCHAR(255) NOT NULL,
+                                                                    bus_type VARCHAR(255) NOT NULL,
+                                                                    duration VARCHAR(50),
+                                                                    departure_time VARCHAR(25),
+                                                                    reaching_time VARCHAR(25),
+                                                                    price DECIMAL(10, 2) NOT NULL,
+                                                                    available_seats INT,
+                                                                    rating DECIMAL(3, 1),
+                                                                    FOREIGN KEY (route_id) REFERENCES route(id)''')
+    
+    if values:
+        insert_values(cursor,'bus_info',attributes=('id', 'route_id', 'bus_name', 'bus_type', 'duration', 
+                                                    'departure_time', 'reaching_time', 'price', 'available_seats', 'rating'),
+                                                    
+                                        values=values)
+
+
+
+
+
+
+
+
+
+
+
+
+
