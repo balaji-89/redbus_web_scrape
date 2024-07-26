b4024005d0442fa30a9a9fd6847185eafd3c3429
balaji-89 <balaji89237@gmail.com>
2024-07-26 12:14:21 +0530

    updated git ignore

--- scrapping_src/code.py
+++ scrapping_src/code.py
@@ -1,10 +1,8 @@
 import time
 import re
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
 def load_page(driver):
     """
